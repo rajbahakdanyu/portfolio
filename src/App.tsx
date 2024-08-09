5bebf7c14f62c05957653a81ea3afa848a48546f
Danyu Rajbahak <rajbahakdanyu@gmail.com>
2024-08-09 20:18:22 +1000

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import Landing from "./pages/Landing";
+import Landing from "./pages/Landing"
 
-const App = () => <Landing />;
+const App = () => <Landing />
 
-export default App;
+export default App
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,24 +1,24 @@
 const Landing: React.FC = () => (
-  <div>
-    <nav>NavBar</nav>
-
     <div>
-      <h1>Welcome to my portfolio</h1>
-      <h2>Have a look around</h2>
+        <nav>NavBar</nav>
+
+        <div>
+            <h1>Welcome to my portfolio</h1>
+            <h2>Have a look around</h2>
+        </div>
+
+        <div>
+            <h2>About Me</h2>
+        </div>
+
+        <div>
+            <h2>Projects</h2>
+        </div>
+
+        <footer>
+            <h2>Footer</h2>
+        </footer>
     </div>
+)
 
-    <div>
-      <h2>About Me</h2>
-    </div>
-
-    <div>
-      <h2>Projects</h2>
-    </div>
-
-    <footer>
-      <h2>Footer</h2>
-    </footer>
-  </div>
-);
-
-export default Landing;
+export default Landing
