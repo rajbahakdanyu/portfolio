bf69cf3a5ae30b04d8b19bd9e9d9bcc3e8963ce4
Danyu Rajbahak <rajbahakdanyu@gmail.com>
2024-08-11 18:09:32 +1000

    added nav bar

---
+++ src/components/HamburgerMenuIcon.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react"
+
+export const HamburgerMenuIcon: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleClick = () => {
+        setIsOpen(!isOpen)
+    }
+
+    return (
+        <button
+            onClick={handleClick}
+            className='flex flex-col justify-center items-center'>
+            <span
+                className={`hamburger-icon ${
+                    isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
+                }`}></span>
+            <span
+                className={`hamburger-icon my-0.5 ${
+                    isOpen ? "opacity-0" : "opacity-100"
+                }`}></span>
+            <span
+                className={`hamburger-icon ${
+                    isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
+                }`}></span>
+        </button>
+    )
+}
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,11 +1,17 @@
-const Landing: React.FC = () => (
-    <div>
-        <nav>NavBar</nav>
+import { HamburgerMenuIcon } from "../components/HamburgerMenuIcon"
+import logo from "/assets/svg/logo-no-background.svg"
 
-        <div>
-            <h1>Welcome to my portfolio</h1>
-            <h2>Have a look around</h2>
-        </div>
+const Landing: React.FC = () => (
+    <div className='min-h-screen dark:bg-slate-800 text-white p-2 no-scrollbar overflow-y-auto'>
+        <nav className='flex w-screen px-6 py-4 items-center justify-between'>
+            <img src={logo} alt='logo' className='max-w-8' />
+            <HamburgerMenuIcon />
+        </nav>
+
+        <section className='flex w-screen items-center justify-center text-center'>
+            <h1 className='font-semibold text-2xl'>Welcome to my portfolio</h1>
+            <h2 className='font-semibold text-xl'>Have a look around</h2>
+        </section>
 
         <div>
             <h2>About Me</h2>
