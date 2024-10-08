2ff89a1083314aebfb25ea376dbafcfcfa6459d7
rajbahakdanyu <rajbahakdanyu@gmail.com>
2024-10-08 15:31:49 +1000

    feat: add framer motion

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => (
     <footer className='bg-gray-800 text-gray-400 py-6'>
         <div className='container mx-auto px-6 text-center'>
             <p>
-                &copy; {new Date().getFullYear()} Your Name. All rights
+                &copy; {new Date().getFullYear()} Danyu Rajbahak. All rights
                 reserved.
             </p>
         </div>
